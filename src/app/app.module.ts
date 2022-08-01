852da9b23c413cef7286c59b74436c8d2f4eda6f
Wilmar Zapata <wavilla@stefanini.com>
2022-08-01 16:14:47 -0500

    Configurar para poder usar el formato fecha en español

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -39,7 +39,7 @@ const ROUTES: Routes = [
     FormsModule,
     RouterModule.forRoot(ROUTES)
   ],
-  providers: [ClienteService],
+  providers: [ClienteService, {provide: LOCALE_ID, useValue: 'es-CO'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
