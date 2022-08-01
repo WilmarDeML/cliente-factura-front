99585ec2f7174580ae6ff28f36b0685084c89fd7
Wilmar Zapata <wavilla@stefanini.com>
2022-08-01 12:28:31 -0500

    Add array de clientes

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,16 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] = [
+  {id: 1, nombre: 'Wilmar', apellido: 'Zapata', email: 'wilmar@wmail.wom', createAt: '2022-05-21'},
+  {id: 2, nombre: 'Migue', apellido: 'Zapata', email: 'migue@wmail.wom', createAt: '2022-06-25'},
+  {id: 3, nombre: 'Isaac', apellido: 'Zapata', email: 'isaac@wmail.wom', createAt: '2022-06-11'},
+  {id: 4, nombre: 'Helen', apellido: 'Villa', email: 'helen@wmail.wom', createAt: '2022-06-05'},
+  {id: 5, nombre: 'Antonio', apellido: 'Zapata', email: 'antonio@wmail.wom', createAt: '2022-06-17'},
+  {id: 6, nombre: 'Manuela', apellido: 'Zapata', email: 'manuela@wmail.wom', createAt: '2022-07-13'},
+  {id: 7, nombre: 'Luciana', apellido: 'Tobón', email: 'luciana@wmail.wom', createAt: '2022-07-08'},
+  {id: 8, nombre: 'Sandra', apellido: 'Gil', email: 'sandra@wmail.wom', createAt: '2022-07-22'},
+  {id: 9, nombre: 'Liliana', apellido: 'Londoño', email: 'liliana@wmail.wom', createAt: '2022-04-25'},
+  {id: 10, nombre: 'Michael', apellido: 'Zapata', email: 'michael@wmail.wom', createAt: '2022-04-10'},
+  {id: 11, nombre: 'Camila', apellido: 'Bohórquez', email: 'camila@wmail.wom', createAt: '2022-04-15'},
+
+]
