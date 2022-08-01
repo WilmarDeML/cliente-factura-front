5b7329617f006eb28e1a613d3268814d65c112fc
Wilmar Zapata <wavilla@stefanini.com>
2022-08-01 15:35:51 -0500

    cambiar nombre a mayúsculas

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,43 +1,51 @@
-import { Injectable } from '@angular/core';
-import { Cliente } from './cliente';
-// import { CLIENTES } from './clientes.json';
-import { Observable, of, throwError, map, catchError } from 'rxjs';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import Swal from 'sweetalert2';
-import { Router } from '@angular/router';
+import { Injectable } from '@angular/core'
+import { Cliente } from './cliente'
+// import { CLIENTES } from './clientes.json'
+import { Observable, of, throwError, map, catchError } from 'rxjs'
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+import Swal from 'sweetalert2'
+import { Router } from '@angular/router'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
 
-  private urlEndPoint:string = 'http://localhost:8080/api/clientes'
+  private urlEndPoint: string = 'http://localhost:8080/api/clientes'
 
-  private httpHeaders = new HttpHeaders({'Content-Type': 'application/json'})
+  private httpHeaders = new HttpHeaders({ 'Content-Type': 'application/json' })
   constructor(private http: HttpClient, private router: Router) { }
 
   getClientes(): Observable<Cliente[]> {
-    // return of(CLIENTES);
-    // return this.http.get<Cliente[]>(this.urlEndPoint); // Sin map
-    return this.http.get(this.urlEndPoint).pipe(map(response => response as Cliente[])); // Con map
+    // return of(CLIENTES)
+    // return this.http.get<Cliente[]>(this.urlEndPoint) // Sin map
+    return this.http.get(this.urlEndPoint).pipe(map(
+      response => {
+        let clientes = response as Cliente[]
+        return clientes.map(cliente => {
+          cliente.nombre = cliente.nombre.toUpperCase()
+          return cliente
+        })
+      }
+    )) // Con map
   }
 
   // Obteniendo sólo el cliente de la respuesta del back
-  create(cliente: Cliente) : Observable<Cliente> {
-    return this.http.post<Cliente>(this.urlEndPoint, cliente, {headers: this.httpHeaders}).pipe(
+  create(cliente: Cliente): Observable<Cliente> {
+    return this.http.post<Cliente>(this.urlEndPoint, cliente, { headers: this.httpHeaders }).pipe(
       map((response: any) => response.cliente as Cliente),
       catchError(e => {
         if (e.status === 400) {
           return throwError(() => e)
         }
-        console.log(e.error.mensaje);
+        console.log(e.error.mensaje)
         Swal.fire(e.error.mensaje, e.error.error, 'error')
         return throwError(() => e)
       })
-    );
+    )
   }
 
-  getCliente(id: number) : Observable<Cliente> {
+  getCliente(id: number): Observable<Cliente> {
     return this.http.get<Cliente>(`${this.urlEndPoint}/${id}`).pipe(
       catchError(e => {
         this.router.navigate(['clientes'])
@@ -45,30 +53,30 @@ export class ClienteService {
         Swal.fire('Error al editar', e.error.mensaje, 'error')
         return throwError(() => e)
       })
-    );
+    )
   }
 
   // Obteniendo toda la respuesta del back
-  update(cliente: Cliente) : Observable<any>{
-    return this.http.put<Cliente>(`${this.urlEndPoint}/${cliente.id}`, cliente, {headers: this.httpHeaders}).pipe(
+  update(cliente: Cliente): Observable<any> {
+    return this.http.put<Cliente>(`${this.urlEndPoint}/${cliente.id}`, cliente, { headers: this.httpHeaders }).pipe(
       catchError(e => {
         if (e.status === 400) {
           return throwError(() => e)
         }
-        console.log(e.error.mensaje);
+        console.log(e.error.mensaje)
         Swal.fire(e.error.mensaje, e.error.error, 'error')
         return throwError(() => e)
       })
-    );
+    )
   }
 
-  delete(id: number) : Observable<any> {
-    return this.http.delete<Cliente>(`${this.urlEndPoint}/${id}`, {headers: this.httpHeaders}).pipe(
+  delete(id: number): Observable<any> {
+    return this.http.delete<Cliente>(`${this.urlEndPoint}/${id}`, { headers: this.httpHeaders }).pipe(
       catchError(e => {
-        console.log(e.error.mensaje);
+        console.log(e.error.mensaje)
         Swal.fire(e.error.mensaje, e.error.error, 'error')
         return throwError(() => e)
       })
-    );;
+    )
   }
 }
