51d0e09cd205b3d9626c1e746bd403b8215c17c5
Wilmar Zapata <wavilla@stefanini.com>
2022-08-22 19:15:39 -0500

    Implementar boton sigin o signout según según el caso

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+import { AuthService } from '../usuarios/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -6,4 +9,14 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   title: string = 'App Angular'
+
+  constructor(public authService: AuthService, private router: Router) {}
+
+  logout(): void {
+    const usuario = this.authService.usuario
+    this.authService.logout()
+    Swal.fire('Logout', `Hola ${usuario.username}, te deslogueaste correctamente`, 'success')
+    this.router.navigate(['/login'])
+  }
+
 }
