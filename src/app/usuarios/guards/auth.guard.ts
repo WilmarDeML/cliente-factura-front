a4b61b19c0b7ff6c4e2c27ccfd3b6d016c7c8e87
Wilmar Zapata <wavilla@stefanini.com>
2022-08-23 11:45:44 -0500

    Validando expiración de token a un nivel front más arriba

--- src/app/usuarios/guards/auth.guard.ts
+++ src/app/usuarios/guards/auth.guard.ts
@@ -13,7 +13,23 @@ export class AuthGuard implements CanActivate {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return this.authService.isAuthenticated() || this.router.navigate(['/login'])
+    if (this.authService.isAuthenticated()) {
+      if (this.isTokenExpired()) {
+        this.authService.logout()
+        this.router.navigate(['/login'])
+        return false
+      }
+      return true
+    }
+    this.router.navigate(['/login'])
+    return false
+  }
+
+  isTokenExpired(): boolean {
+    const token = this.authService.token
+    const payload = this.authService.obtenerDatosToken(token)
+    const now = new Date().getTime() / 1000
+    return payload.exp < now
   }
 
 }
