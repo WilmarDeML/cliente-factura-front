a9d14406222526d26f0d49294c8feda55e1b58b7
Wilmar Zapata <wavilla@stefanini.com>
2022-09-29 01:27:53 -0500

    Cambiando de private a public  modalService

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -20,7 +20,7 @@ export class ClientesComponent implements OnInit {
   constructor(
     private clienteService: ClienteService,
     private activateRoute: ActivatedRoute,
-    private modalService: ModalService,
+    public modalService: ModalService,
     public authService: AuthService) { }
 
   ngOnInit(): void {
